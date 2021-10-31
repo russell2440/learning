5edfa72ba2d032ca8efc21e73455c0419535fab5
Russell Shahenian <russ85226@gmail.com>
2021-10-31 07:20:36 -0700

    update

--- udemy/andrein/algs_ds/merge_sort/merge_sort_test.cpp
+++ udemy/andrein/algs_ds/merge_sort/merge_sort_test.cpp
@@ -15,7 +15,10 @@ int main() {
 	int numbers[] = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
 	vector<int> vn(numbers, numbers+(sizeof(numbers)/sizeof(*numbers)));
 
-	cout <<"!!!Hello Russ!!!: " << __PRETTY_FUNCTION__ << endl; // prints !!!Hello World!!!
+	cout
+	<< "!!!Hello Russell : Dont Use No Matter What : Mind Your Own Business and Shut Your Mputh and Listen !!!: "
+	<< endl
+	<< __PRETTY_FUNCTION__ << endl; // prints !!!Hello World!!!
 	merge_sort ms;
 	ms.run(vn);
 	return 0;
